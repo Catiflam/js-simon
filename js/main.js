d81429a2c30bf25f13d15c2ede49755c477f8009
Catiflam <gilfrid1@gmail.com>
2023-08-23 19:40:53 +0200

    edit const

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const timeClock = document.getElementById("title");
+const timeDays = document.getElementById("days");
+const timeHours = document.getElementById("hours");
+const timeMinutes = document.getElementById("minutes");
+const timeSeconds = document.getElementById("seconds");
+
+const countdown = setInterval(function () {});
