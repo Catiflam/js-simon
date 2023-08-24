617c4408043c93bca1abfc676330bc5444548637
Catiflam <gilfrid1@gmail.com>
2023-08-24 14:11:40 +0200

    edit countdown

--- js/main.js
+++ js/main.js
@@ -4,4 +4,23 @@ const timeHours = document.getElementById("hours");
 const timeMinutes = document.getElementById("minutes");
 const timeSeconds = document.getElementById("seconds");
 
-const countdown = setInterval(function () {});
+const countdown = setInterval(function () {
+	const now = new Date();
+	const tomorrow = new Date("23-08-25 9:30");
+
+	const msNow = now.getTime();
+
+	const msTomorrow = tomorrow.getTime();
+
+	const sToTomorrow = Math.floor((msTomorrow - msNow) / 1000);
+
+	const seconds = sToTomorrow % 60;
+	const minutes = Math.floor((sToTomorrow / 60) % 60);
+	const hours = Math.floor((sToTomorrow / 60 / 60) % 24);
+	const days = Math.floor(sToTomorrow / 60 / 60 / 24);
+
+	timeSeconds.innerText = seconds < 10 ? "0" + seconds : seconds;
+	timeMinutes.innerText = minutes < 10 ? "0" + minutes : minutes;
+	timeHours.innerText = hours < 10 ? "0" + hours : hours;
+	timeDays.innerText = days < 10 ? "0" + days : days;
+});
